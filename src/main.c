70cd5f072a05a3926d6fdfeb13b1c724fdec6088
Kálley Wilkerson <yellakkalley@gmail.com>
2019-04-25 20:46:13 -0300

    Adicionado comentários explicativos na main.c e a documentação do projeto

--- src/main.c
+++ src/main.c
@@ -1,5 +1,13 @@
 #include "../include/test_mem.h"
 
+/* Sistema Operacional utilizado: Linux Ubuntu 18.04
+   Compilador: GCC 
+   Editor: Emacs (o programa não está atrlado a uma IDE específica
+
+   Como compilar:
+   No terminal, dentro da pasta src deste projeto digite "make", o
+   programa irá compilar e então execute digitando ./main */
+
 int main() {
   printf("Executando testes iniciais:\n");
   run_tests();
