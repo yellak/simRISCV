5503c208ef93a4a693d16f4a5e78029d19a857cf
Kálley Wilkerson <yellakkalley@gmail.com>
2019-05-05 17:10:53 -0300

    Criada interação com o usuário

--- src/main.c
+++ src/main.c
@@ -9,10 +9,34 @@
    programa irá compilar e então execute digitando ./main */
 
 int main() {
+  char option;
   init_simulator();
   read_mem(data_mem, "progs/BJ_data.bin");
   read_mem(memory, "progs/BJ.bin");
   load_data();
-  run();
+
+  printf("Iniciando o programa...\n");
+  while(END != 1) {
+    printf("\n--------------------\n");
+    printf("Escolha uma:\n");
+    printf("s-step\n");
+    printf("r-run\n");
+    printf("m-mostrar registros\n");
+    scanf("\n%c", &option);
+    switch(option) {
+    case 's':
+      step();
+      break;
+
+    case 'r':
+      run();
+      break;
+
+    case 'm':
+      dump_reg('h');
+      break;
+    } /* switch(option) */
+  }
+
   printf("\n");
 }
